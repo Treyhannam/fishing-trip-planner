083d1a3defd257a51ed57829db93d2e51338c42c
Trey <hannamtrey@gmail.com>
2023-12-28 12:36:26 -0700

    ADD: recreate master angler table

--- sql_pending/2_2023_12_create_master_angler_table.sql
+++ sql_pending/2_2023_12_create_master_angler_table.sql
@@ -1,5 +1,6 @@
 create or replace table STORAGE_DATABASE.CPW_DATA.MASTER_ANGLER_AWARD (
     master_angler_award_id NUMBER(38,0) autoincrement start 0 increment by 1,
+	"main_species" VARCHAR(100),
 	"angler" VARCHAR(1000),
 	"species" VARCHAR(100),
 	"length" NUMBER(2, 0),
