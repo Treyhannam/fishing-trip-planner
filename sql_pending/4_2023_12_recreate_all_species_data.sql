2413df42f227cd9970a948b9d022ff3bcb8efb92
Trey <hannamtrey@gmail.com>
2023-12-28 12:43:05 -0700

    EDIT: add new column

--- sql_pending/4_2023_12_recreate_all_species_data.sql
+++ sql_pending/4_2023_12_recreate_all_species_data.sql
@@ -1,5 +1,6 @@
 create table if not exists STORAGE_DATABASE.CPW_DATA.STORAGE_DATABASE.CPW_DATA.ALL_SPECIES (
     all_species_id NUMBER(38,0) autoincrement start 0 increment by 1,
+	"main_species" VARCHAR(100),
 	"angler" VARCHAR(1000),
 	"species" VARCHAR(100),
 	"length" NUMBER(2, 0),
