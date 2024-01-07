3ed6886d1cd2010a3264e3a27aed1e8869614c45
Trey <hannamtrey@gmail.com>
2024-01-07 09:53:49 -0700

    EDIT: uppercase col names

--- fishing_trip_planning_sql_files/3_deploy_proceedure/3_insert_date_from_temp_to_master.sql
+++ fishing_trip_planning_sql_files/3_deploy_proceedure/3_insert_date_from_temp_to_master.sql
@@ -7,10 +7,10 @@ INSERT INTO STORAGE_DATABASE.CPW_DATA.MASTER_ANGLER_AWARD (
 	"released"
 )
 SELECT
-    "angler",
-	"species",
-	"length",
-	"location",
-	"date",
-	"released"
+    "Angler",
+	"Species",
+	"Length",
+	"Location",
+	"Date",
+	"Released"
 from STORAGE_DATABASE.CPW_DATA.MASTER_ANGLER_AWARD_TEMP
