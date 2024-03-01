34b341f77d3b3bd699a528a202dc4a9bee78f6b2
Trey <hannamtrey@gmail.com>
2024-02-29 19:32:57 -0700

    EDITl: fix test

--- tests/snowflake_/test_trout_pattern_match.py
+++ tests/snowflake_/test_trout_pattern_match.py
@@ -5,10 +5,11 @@ def test_clean_data():
     test = pd.DataFrame(
     {
         'wrong' : ['11 mile', '11mile', 'deckers', 'cheesman canyon', 'cheeseman', 'dream stream', 'treys pond', 'resevoir', 'north fork south platte', 'spinney', 'Spinney mountain()'],
-        'right' : ['eleven mile', 'eleven mile', 'south platte', 'south platte', 'cheesman', 'south platte', 'treys', '', 'south platte',  'spinney mountain', 'spinney mountain']
+        'right' : ['elevenmile', 'elevenmile', 'south platte', 'south platte', 'cheesman', 'south platte', 'treys', '', 'south platte',  'spinney mountain', 'spinney mountain']
     }
     )
 
     trout_pattern_match._clean_data(test, cols=['wrong'])
 
-    assert all(test.right == test.wrong_clean)
+    for row in test.itertuples():
+        assert row.right == row.wrong_clean
